58ce7cbe0a55243162e30f189c5ce21bd57f15ee
Rahul Bhoyar <rahulbhoyraoffice@gmail.com>
2024-04-28 22:10:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+---
+title: Email Generator using LLM
+emoji: 🚀
+colorFrom: purple
+colorTo: green
+sdk: streamlit
+sdk_version: 1.33.0
+app_file: app.py
+pinned: false
+---
+
+# Email Generator using LLM
+
+#### Author: Rahul Bhoyar
+
+Welcome to Email Generator using Large Language Models (LLM). This repository hosts an application that utilizes state-of-the-art natural language processing techniques to generate emails automatically.
+
+## Features:
+
+- Utilizes Large Language Models (LLMs) for generating email content.
+- Provides an efficient way to create personalized and effective email drafts.
+- Customizable parameters for generating emails based on user preferences.
+- User-friendly interface for easy interaction.
+
+
---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from llama_index import VectorStoreIndex
+from llama_index import ServiceContext
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index.llms import HuggingFaceInferenceAPI
+from llama_hub.youtube_transcript import YoutubeTranscriptReader
+
+# Streamlit title and description
+st.title("Youtube Video Content Querier by Rahul Bhoyar")
+st.write("Base Model : **HuggingFaceH4/zephyr-7b-alpha (open-source from HuggineFace)**")
+st.write("Embedding Model : **WhereIsAI/UAE-Large-V1(open-source from HuggineFace)**")
+st.write("This app allows you to read the Youtube video content and tal with it.")
+
+# Streamlit input for user file upload
+youtube_url = st.text_input("Enter the youtube video url:")
+
+# Load data and configure the index
+
+    # input_file = save_uploadedfile(uploaded_pdf)
+    # st.write("File uploaded successfully!")
+    # documents = SimpleDirectoryReader("data").load_data()
+if youtube_url:#is not None: 
+#if youtube_url is not None: 
+    loader = YoutubeTranscriptReader()
+    documents = loader.load_data(ytlinks=[youtube_url])
+    st.success("Documents loaded successfully!")
+    st.markdown("**Here is the content of the Youtube Video:**")
+    st.markdown("--" * 15)
+    st.markdown(dict(documents[0])['text'])
+    st.markdown("--" * 12)
+
+
+    with st.spinner('Creating Vector Embeddings...'):
+        llm = HuggingFaceInferenceAPI(model_name="HuggingFaceH4/zephyr-7b-alpha")
+        embed_model_uae = HuggingFaceEmbedding(model_name="WhereIsAI/UAE-Large-V1")
+
+
+        service_context = ServiceContext.from_defaults(
+            llm=llm, chunk_size=800, chunk_overlap=20, embed_model=embed_model_uae
+        )
+        index = VectorStoreIndex.from_documents(documents, service_context=service_context, show_progress=True)
+        index.storage_context.persist()
+        query_engine = index.as_query_engine()
+        # Display the result of the task
+    st.success("Vector embeddings created.")
+
+    # Streamlit input for user query
+    user_query = st.text_input("Enter your query:")
+
+    # Query engine with user input
+    if user_query:
+        with st.spinner('Fetching the response...'):
+            response = query_engine.query(user_query)
+            
+        st.markdown(f"**Response:** {response}")
